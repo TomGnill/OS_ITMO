1f6afa8a3883b71e31dba330843df9b70bf0bf1d
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-10-26 12:17:19 +0300

    2.6

--- lab2/topmemproc.sh
+++ lab2/topmemproc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Top memory proc:"
-grep "VmRSS" /proc/*/status |head -n -2 | sed "s/[^0-9]\+/ /g" | sed "s/^ //" | sort -nrk2 | head -1 | awk '{print "PID: "$1" RES: "$2"}'
+grep "VmRSS" /proc/*/status | head -n -2 | sed "s/[^0-9]\+/ /g" | sed "s/^ //" | sort -nrk2 | head -1 | awk '{print "PID: "$1" RES: "$2"}'
