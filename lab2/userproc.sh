95ade39b7853381deb6e463fe15f572028fc713c
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-10-26 12:15:27 +0300

    2.1

--- lab2/userproc.sh
+++ lab2/userproc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 proc=$(ps -U user -o pid,command | tail -n +2 | sed -r "s/\s*([^ ]+)\s(.*)/\1:\2/")
 lines_cnt=$(echo "$proc"|wc -1)
 echo "Total process num: $lines_cnt" > userproc.txt
