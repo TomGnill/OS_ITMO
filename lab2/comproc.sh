987fcdd126471c4999599f0cc287fb51d0675d86
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-10-26 12:19:10 +0300

    2.2

--- lab2/comproc.sh
+++ lab2/comproc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ps -Ao pid, command  | tail -n +2 | awk '{if ($2 ~ /^\/sbin\//)print $1}' > commproc.txt
+ps -Ao pid,command | tail -n +2 | awk '{if ($2 ~ /^\/sbin\//)print $1}' > commproc.txt
 
