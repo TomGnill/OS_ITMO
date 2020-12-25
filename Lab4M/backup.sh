1fc82792440e904dda16f32832eea4a2e53ccf0b
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-12-25 16:48:11 +0300

    upd Lab4/backup.sh

--- Lab4M/backup.sh
+++ Lab4M/backup.sh
@@ -4,47 +4,42 @@ HOMEDIR="/home/user"
 
 LastBackupDate=$(ls $HOMEDIR | grep -E "^Backup-" | sort -n | tail -1 | sed "s/Backup-//")
 LastBackup="$HOMEDIR/Backup-$(LastBackupDate)"
-Rep=$HOMEDIR/.backup-report
-LastBackupTime=$(date -d "LastBackupDate" +"%s")
 
 NowDate=$(date +"%Y-%m-%d")
-NowTime=$(date -d "$NowDate" +"%s")
 
-CalcTime=$(echo "(${NowTime} - ${LastBackupTime}) / 60 / 60 / 24" | bc )
-
-if [[ $CalcTime > 7 || -z "$LastBackupDate" ]];
+if [[ $(echo "(${$(date -d "$NowDate" +"%s")} - ${$(date -d "LastBackupDate" +"%s")}) / 60 / 60 / 24" | bc ) > 7 || -z "$LastBackupDate" ]];
 then
 	mkdir "$HOMEDIR/Backup-${NowDate}"
-	for file in $(ls $HOMEDIR/source);
-	do
-		cp "$HOMEDIR/source/$file" "$HOMEDIR/Backup-$NowDate"
-	done
-	flist=$(ls $HOMEDIR/source | sed "s/^/\t/")
-	echo -e "${NowDate}. Created:\n${flist}" >> $Rep
+	   for file in $(ls $HOMEDIR/source); do
+		   cp "$HOMEDIR/source/$file" "$HOMEDIR/Backup-$NowDate"
+	   done
+	   
+	      flist=$(ls $HOMEDIR/source | sed "s/^/\t/")
+	      echo -e "${NowDate}. Created:\n${flist}" >> $HOMEDIR/.backup-report
+	      
 else
 	changes=""
-	for file in $(ls $HOMEDIR/source);
-	do
-		if [[ -f "LastBackup/$file" ]];
-	then
+	 for file in $(ls $HOMEDIR/source); do
+	      if [[ -f "LastBackup/$file" ]];
+	      then
 		sourceSize=$(wc -c "$HOMEDIR/source/${file}" | awk '{print $1}')
 		BackupSize=$(wc -c "${LastBackup}/${file}" | awk '{print $1}')
 		Size=$(echo "${sourceSize} - ${BackupSize}" | bc )
 
-		if [[ $Size != 0 ]];
-		then
-			mv "$LastBackup/$file" "$LastBackup/$file.NowDate"
-			cp "HOMEDIR/source/$file" $LastBackup
-			changes="${changes}\n\t$file ($file.$NowDate)"
-		fi
-		else
-			cp "$HOMEDIR/source/$file" $LastBackup
-			changes="${changes}\n\t$file"
-	fi
+			if [[ $Size != 0 ]];
+			then
+			    mv "$LastBackup/$file" "$LastBackup/$file.NowDate"
+			    cp "HOMEDIR/source/$file" $LastBackup
+			    changes="${changes}\n\t$file ($file.$NowDate)"
+			fi
+	      else
+ 		cp "$HOMEDIR/source/$file" $LastBackup
+		changes="${changes}\n\t$file"
+	        fi
 	done
     changes=$(echo $changes | sed 's/^\\n//')
-    if[[ ! -z "$changes"]];
+    if[[ ! -z "$changes" ]];
     then
-	  echo -e "${LastBackupDate}. Updated:\n${changes}" >> $Rep
+	  echo -e "${LastBackupDate}. Updated:\n${changes}" >> $HOMEDIR/.backup-report
        fi
 fi
