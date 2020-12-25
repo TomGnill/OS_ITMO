f80f1728891bdb94111b198f2dd75a5685ad17b3
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-12-25 17:43:58 +0300

    Update rmtrash.sh

--- Lab4M/rmtrash.sh
+++ Lab4M/rmtrash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LABDIR = "/home/user/Lab4M"
+TRASHDIR = "/home/user/.trash"
 if[[ $# > 1 || $# < 1 || ! -f $1 ]];
 then
 	echo "Error in arg."
@@ -8,17 +10,19 @@ fi
 if [[ ! -e "/home/user/.trash" ]];
 then
 	mkdir /home/user/.trash
+	
 fi
 
-value=$(find "/home/user/.trash/" -type f -name "[1-9]" | sed -e 's,.trash/,,')
+value=$(find "$TRASHDIR/" -type f -name "[1-9]" | sed -e 's,.trash/,,')
 value=$(echo $value | awk '{print NF}')
 
 if [ -z $value ];
 then
-	ln "/home/user/Lab4M/"$1 "/home/user/.trash/1"
+	ln "$LABDIR/"$1 "$TRASHDIR/1"
 else
 	value=$(($value + 1))
-	ln "/home/user/Lab4M/"$1 "/home/user/.trash/1"
+	ln "$LABDIR/"$1 "$TRASHDIR/"$value
 fi
+
 echo $(readlink -f $1) $value >> /home/user/.trash.log
 rm $1
