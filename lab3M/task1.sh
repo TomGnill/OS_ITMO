ba6ece52d8a4093c59a1d4d4220be49f84a20133
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-12-25 17:03:29 +0300

    Update task1.sh

--- lab3M/task1.sh
+++ lab3M/task1.sh
@@ -3,4 +3,4 @@
 
 
 mkdir ~/test && { echo "catalog test was created successfully" > ~/report ; touch ~/test/$(date +'%F_%T'); }
-ping net_nikogo.ru || echo "${dates} ERROR HOST" >> ~/report
+ping net_nikogo.ru || echo "${date +'%F_%T'} ERROR HOST" >> ~/report
