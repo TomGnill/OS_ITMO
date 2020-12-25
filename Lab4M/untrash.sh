39a486c129a77387914dd92a75441eb4b65351d2
Andrey <62840086+TomGnill@users.noreply.github.com>
2020-12-25 18:39:11 +0300

    Update untrash.sh

--- Lab4M/untrash.sh
+++ Lab4M/untrash.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-TrashDir="/home/user/.trash/"
-TrashLog="/home/user/.trash.log"
+TrashDir=~/.trash
+TrashLog=~/.trash.log
 HOMEDIR="/home/user"
-file=$1
-a=""
+name=$1
 
 if [[ $# > 1 || $# < 1 || ! -d $TrashDir || ! -f $TrashLog || ! "`echo $1 | sed 's/[a-z,0-9,_,-]*//'`" == "" ]];
 then
@@ -12,58 +11,35 @@ then
 	exit 1
 fi
 
-
-for i in $(grep "$1" $TrashLog | awk '{print $1}');
-do
-	a=$(echo $i | sed -e 's,/home/user/Lab4M/,,')
-	if [ $a == $file ];
-	then
-	    break;
-	fi
-done
 if [ $a != $file]; then
 	echo "Error"
 	exit 1
 fi
 
-for i in $(grep "$1" $TrashLog | awk '{print $NF}');
-do
-	FN=$(grep $i $TrashLog | awk '{$NF=""; print $0}')
-	FN=$(echo "$FN" | sed 's/ *$//')
+restore() {
+	path=$1
+	file=$2
+	ln $TrashDir/$file $path
+}
+
+grep $name $TrashLog |
+while read filepath; do
+         file=$(echo $filepath | cut -d " " -f 1)
+    	 del=$(echo $filepath | cut -d " " -f 2)
+	 
         read -p "${FN} Continue?" yn
         case "$yn" in
 		"y")
-	    	 NFN=""
-	    	 TN=$i
-	    	 RDIR=$(echo "$FN" | awk 'BEGIN{FS=OFS="/"}; {NF=""; print $0}')
-	    	 FNN=$(echo "$FN" | awk 'BEGIN{FS="/"}; {print $NF}')
-	    	 if [[ ! -d $RDIR ]];
-	    	 then
-			echo "Dir not found. File restored in homeDir"
-			if [[ -f "${HOMEDIR}/${FNN}" ]];
-			then
-			    read -p "File \"${HOMEDIR}/${FNN}\" exitst.New name: " NFN
-			    ln "${TrashDir}/${TN}" "${HOMEDIR}/${NFN}"
-	        	    rm "${TrashDir}/${TN}"
-			else
-		 	   ln "${TrashDir}/${TN}" "${HOMEDIR}/${FNN}"
-		 	   rm "${TrashDir}/${TN}"
-		fi
-	         else
-			if [[ -f "${FN}" ]];
-			then
-		 	   read -p "File \"${FN}\" exits. New name: " NFN
-		 	   ln "${TrashDir}/${TN}" "${HOMEDIR}/${NFN}"
-		  	   rm "${TrashDir}/${TN}"
-	       		 else
-			   ln "${TrashDir}/${TN}" "${FN}"
-		  	   rm "${TrashDir}/${TN}"
-	      		 fi
-	           fi
-	  	    sed "/${i}/d" $TrashLog > .trash.log2 && mv .trash.log2 $TrashLog
-	   	    echo "Fine"
-	     	    continue
-	 	   ;;
+	    	dir=$(dirname $file) &&
+       		 if [ -d $dir ]; then
+    	      	     $(restore $file $del)
+     		  else
+       		     $(restore $HOMEDIR/$name $del) &&
+       	  	     echo "Restored in $HOMEDIR"
+       		 fi &&
+        		rm $TrashDir/$del && {
+            		sed -i "#$filepath#d" $TrashLog
+            		echo "Restored $file"
 	 	   *)
           	    echo "Bad choice"
 	  	    continue
